196080ed74b8519ebfa71425891adfcad3598194
Eric Chen <kych@berkeley.edu>
2023-06-20 22:40:20 +0000

    add in change in dist

--- fogros2/fogros2/lambda_service_proxy_client.py
+++ fogros2/fogros2/lambda_service_proxy_client.py
@@ -18,7 +18,8 @@ class MinimalClientAsync(Node):
 
     def send_request(self):
         with open("/tmp/pickled_request", "rb") as f:
-            self.req = f.read()
+            self.req = pickle.loads(f.read())
+        print(self.req.a, self.req.b)
         self.future = self.cli.call_async(self.req)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
